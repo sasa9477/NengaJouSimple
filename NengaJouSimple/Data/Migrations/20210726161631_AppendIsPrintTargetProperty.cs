c4b280e5eca1c2863814982380014e8c8369e157
惣汰 笹田 <sasa9477@outlook.com>
2021-07-27 01:17:55 +0900

    DbContextMigrationでAddressCard_IsPrintTargetのプロパティを追加

---
+++ NengaJouSimple/Data/Migrations/20210726161631_AppendIsPrintTargetProperty.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NengaJouSimple.Data.Migrations
+{
+    public partial class AppendIsPrintTargetProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrintTarget",
+                table: "AddressCards",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPrintTarget",
+                table: "AddressCards");
+        }
+    }
+}
